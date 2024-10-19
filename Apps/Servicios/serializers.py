e2e8a48361f6bd19e8d70e16bcb9edb5d27fcf0a
Sebastian Arias Rodriguez <46092477+Crowned-Ax@users.noreply.github.com>
2024-10-18 19:32:17 -0500

    cambios a servicios

--- Apps/Servicios/serializers.py
+++ Apps/Servicios/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Services
+from .models import Services, ClientService
 
 class ServicesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Services
-        fields = ['id', 'name', 'description', 'url', 'img', 'client']
+        fields = "__all__"
+
+class ClientServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClientService
+        fields = ['client', 'service']
+
--- Apps/urls.py
+++ Apps/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .Servicios.views import ServicesViewSet
+
+router = DefaultRouter()
+router.register(r'services', ServicesViewSet)
 
 urlpatterns = [
-    path('Users/', include('Apps.Usuario.urls')),
-    path('Agenda/', include('Apps.Agenda.urls')),
-    path('Clients/', include('Apps.Clientes.urls')),
+    path('users/', include('Apps.Usuario.urls')),
+    path('agenda/', include('Apps.Agenda.urls')),
+    path('clients/', include('Apps.Clientes.urls')),
+    path('', include(router.urls)),
 ]
