55aca56bf82ba384dac6f7162b501f18f5f27899
Sebastian AR <crownedax@gmail.com>
2024-10-18 23:37:59 -0500

    servicios 2

--- Apps/Servicios/views.py
+++ Apps/Servicios/views.py
@@ -1,27 +1,29 @@
-from rest_framework import generics
-from .models import Services
-from django.shortcuts import get_object_or_404
-from .serializers import ServicesSerializer
-from ..Clientes.models import Client
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Client, Services, ClientService
+from .serializers import ClientSerializer, ServicesSerializer, ClientServiceSerializer
 
-# Vista para listar y crear Servicios
-class ServicesListCreateView(generics.ListCreateAPIView):
-    serializer_class = ServicesSerializer
-    def get_queryset(self):
-        client_id = self.kwargs['client_id']
-        return Services.objects.filter(client__id=client_id)
-
-    def perform_create(self, serializer):
-        client_id = self.kwargs['client_id']
-        client = get_object_or_404(Client, id=client_id)
-        serializer.save(client=client)
-
-# Vista para obtener, actualizar y eliminar un servicio específico
-class ServicesDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ServicesViewSet(viewsets.ModelViewSet):
     queryset = Services.objects.all()
     serializer_class = ServicesSerializer
 
-    def get_queryset(self):
-        client_id = self.kwargs['client_id']
-        return Services.objects.filter(client__id=client_id)
+class ClientServiceViewSet(viewsets.ViewSet):
+    def create(self, request, client_id=None):
+        serializer = ClientServiceSerializer(data=request.data)
+        if serializer.is_valid():
+            # Aseguramos que el cliente exista
+            try:
+                serializer.validated_data['client'] = Client.objects.get(id=client_id)
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except Client.DoesNotExist:
+                return Response({'error': 'Client not found.'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def destroy(self, request, client_id=None, pk=None):
+        try:
+            client_service = ClientService.objects.get(id=pk, client_id=client_id)
+            client_service.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ClientService.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
--- Apps/Usuario/urls.py
+++ Apps/Usuario/urls.py
@@ -4,9 +4,9 @@ from .views import LoginView, RegisterView, UserDetailView, ChangePasswordView, 
 app_name = 'usuario'
 
 urlpatterns = [
-    path("", UserListView.as_view(), name='user-list'),
-    path("login/", LoginView.as_view(), name="login"),
-    path("signup/", RegisterView.as_view(), name="register"),
-    path("user/<str:email>/", UserDetailView.as_view(), name ="profile"),
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path("", UserListView.as_view(), name='user-list'), # Traer todos los usuarios
+    path("login/", LoginView.as_view(), name="login"), # Loguearse
+    path("signup/", RegisterView.as_view(), name="register"), # Registrarse
+    path("user/<str:email>/", UserDetailView.as_view(), name ="profile"), #traer la info de un usuario segun su correo electronico
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'), # Cambiar la contraseña del usuario logueado
 ]
