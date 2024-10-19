8494a9a5ee6932b9777d5b73fd3bfc0a69e151ec
Sebastian Arias Rodriguez <46092477+Crowned-Ax@users.noreply.github.com>
2024-10-18 23:18:15 -0500

    fix 1

--- Apps/Servicios/views.py
+++ Apps/Servicios/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Client, Services, ClientService
-from .serializers import ClientSerializer, ServicesSerializer, ClientServiceSerializer
+from .serializers import ServicesSerializer, ClientServiceSerializer
 
 class ServicesViewSet(viewsets.ModelViewSet):
     queryset = Services.objects.all()
