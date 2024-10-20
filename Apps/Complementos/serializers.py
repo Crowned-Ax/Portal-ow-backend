51b8843f1e6aa54f32c98f9208d7d6925ade4753
Sebastian AR <crownedax@gmail.com>
2024-10-20 15:12:28 -0500

    🐛 importacion invalida 2

--- Apps/Complementos/serializers.py
+++ Apps/Complementos/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Complements, ClientComplements
+from .models import Complements
 
 class ComplementSerializer(serializers.ModelSerializer):
     class Meta:
