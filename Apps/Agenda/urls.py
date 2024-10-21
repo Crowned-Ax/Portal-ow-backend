d2c7897a30958d6624aa8282811d491346566e12
Sebastian AR <crownedax@gmail.com>
2024-10-21 18:41:56 -0500

    prueba url 2

--- Apps/Agenda/urls.py
+++ Apps/Agenda/urls.py
@@ -6,15 +6,15 @@ from .views import ScheduleViewSet
 router = DefaultRouter()
 
 # Registrar el ViewSet en el router
-router.register(r'', ScheduleViewSet, basename='schedule')
+router.register(r'', ScheduleViewSet)
 
 # Incluir las URLs del router
 urlpatterns = [
     path('', include(router.urls)),
 ]
 
-# las url se autogeneran aqui y son sencillas, basandose en la raiz "schedule"
+# las url se autogeneran aqui y son sencillas, basandose en la raiz "Agenda"
 # solo es cambiar los distintos tipos de peticion (POST, GET, PUT, etc)
 # y en los casos donde se requiere especificar como un PUT o DELETE
-# GET /schedule/{id}/
+# GET /Agenda/{id}/
 # es solo poner el id del item especifico
--- Apps/urls.py
+++ Apps/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,  include
 
 urlpatterns = [
     path('users/', include('Apps.Usuario.urls')),
-    path('', include('Apps.Agenda.urls')), #schedule
+    path('agenda', include('Apps.Agenda.urls')),
     path('clients/', include('Apps.Clientes.urls')),
     path('', include('Apps.Servicios.urls')), #services
     path('complements/', include('Apps.Complementos.urls')), #complements
