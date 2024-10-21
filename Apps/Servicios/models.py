980ceedc431c3ae512ebd698696abe65be76fb91
Sebastian AR <crownedax@gmail.com>
2024-10-20 19:19:11 -0500

    modelo servicios

--- Apps/Servicios/models.py
+++ Apps/Servicios/models.py
@@ -9,7 +9,7 @@ SER_CATEGORY = [
 class Services(models.Model):
     name = models.CharField(max_length=40, blank=True)
     description = models.TextField(blank=True)
-    img = models.ImageField(blank=True)
+    img = models.ImageField(upload_to='images/', blank=True, null=True)
     category = models.CharField(
         choices=SER_CATEGORY, 
         default='OK',
