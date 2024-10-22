e61896ce5f96a0080b29794acd0663d853f6fbd6
Sebastian AR <crownedax@gmail.com>
2024-10-21 21:22:33 -0500

    ajustes a usuario/colaborador

--- Apps/Usuario/models.py
+++ Apps/Usuario/models.py
@@ -5,6 +5,7 @@ from .managers import UserManager
 IDENTIFICACION_OPCIONES = [
     ('CC', 'Cédula de ciudadanía'),
     ('TI', 'tarjeta de identidad'),
+    ('CE', 'Cédula de extranjería'),
     ('P', 'Pasaporte'),
 ]
 
@@ -14,21 +15,24 @@ class User(AbstractBaseUser, PermissionsMixin):
     # Informacion general
     name = models.CharField(max_length=30, blank=True)
     lastname = models.CharField(max_length=30, blank=True)
-    tel = models.CharField(max_length=15, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
     eEmail_personal = models.EmailField(max_length=40, blank=True)
     eEmail_laboral = models.EmailField(max_length=40, blank=True)
     birthday = models.DateField(null=True)
-    id_type = models.CharField(
+    documentType = models.CharField(
         choices=IDENTIFICACION_OPCIONES, 
         default='CC',
         max_length=3,
         verbose_name='Tipo de documento'
     )
+    documentNumber = models.CharField(max_length=15, blank=True)
     country = models.CharField(max_length=47, blank=True)
     state = models.CharField(max_length=40, blank=True)
     city = models.CharField(max_length=40, blank=True)
     address = models.CharField(max_length=50, blank=True)
     # campos de Cv
+    position = models.CharField(max_length=40, blank=True)
+    description = models.TextField(blank=True,null=True)
     language = models.CharField(max_length=40, blank=True)
     formation = models.CharField(max_length=50, blank=True)
     knowledge = models.CharField(max_length=50, blank=True)
