87b9576430fde1b86c835bc7b29df853686a25f1
Sebastian AR <crownedax@gmail.com>
2024-10-21 21:34:10 -0500

    ajustes menores y migraciones

--- src/settings.py
+++ src/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2he@%9q+e38u8!t1*oi4f&4-4_zk-_v0d0hwsa1v^skoc9=^8i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.getenv("ENVIRONMENT") == "development"
+DEBUG = True 
+#os.getenv("ENVIRONMENT") == "development"
 
 ALLOWED_HOSTS = ["*"]
 
