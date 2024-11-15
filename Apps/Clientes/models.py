9d940b4b2575effc88d192b8a482cba47e17980f
Sebastian AR <crownedax@gmail.com>
2024-11-15 17:20:27 -0500

    🐛 Ajuste campo photo en clientes

--- Apps/Clientes/models.py
+++ Apps/Clientes/models.py
@@ -56,7 +56,7 @@ class Client(models.Model):
     address = models.CharField(max_length=30, blank=True)
     phone = models.CharField(max_length=15, blank=True)
     position = models.CharField(max_length=30, blank=True)
-    photo = models.CharField(max_length=40, blank=True, null=True)
+    photo = models.URLField(blank=True,null=True)
     # Informacion tributaria
     mark_name = models.CharField(max_length=30, blank=True)
     corporate_name = models.CharField(max_length=30, blank=True) 
