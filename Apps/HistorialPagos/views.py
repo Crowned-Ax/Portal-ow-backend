7111c22736d1d206ad46375e6cb2dc24a991de11
Sebastian AR <crownedax@gmail.com>
2024-11-27 20:58:23 -0500

    ⚡️ cambios al filtrado de historial de pagos

--- Apps/HistorialPagos/views.py
+++ Apps/HistorialPagos/views.py
@@ -10,18 +10,15 @@ from ..Usuario.models import User
 class PaymentHistoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = PaymentHistory.objects.all()
     serializer_class = HistorySerializer
-
-    @action(detail=False, methods=['get'], url_path='client/(?P<client_id>[^/.]+)')
-    def by_client(self, request, client_id=None):
-        client = get_object_or_404(Client, id=client_id)
-        payments = PaymentHistory.objects.filter(client=client)
+    @action(detail=False, methods=['get'], url_path='clients/')
+    def by_clients(self, request):
+        payments = PaymentHistory.objects.filter(collaborator=None)
         serializer = self.get_serializer(payments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    @action(detail=False, methods=['get'], url_path='collaborator/(?P<collabo_id>[^/]+)')
-    def by_collaborator(self, request, collabo_id=None):
-        collabo = get_object_or_404(User, email=collabo_id)
-        payments = PaymentHistory.objects.filter(collaborator=collabo)
+    @action(detail=False, methods=['get'], url_path='collaborator/')
+    def by_collaborators(self, request):
+        payments = PaymentHistory.objects.filter(client=None)
         serializer = self.get_serializer(payments, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
