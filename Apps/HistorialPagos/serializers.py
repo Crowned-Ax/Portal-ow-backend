fc3a87a00babcba64540c251f782c094ff646e95
Sebastian AR <crownedax@gmail.com>
2024-11-27 21:37:00 -0500

    ⚡️ se agrego nombres detallados en History

--- Apps/HistorialPagos/serializers.py
+++ Apps/HistorialPagos/serializers.py
@@ -3,6 +3,20 @@ from .models import PaymentHistory
 
 class HistorySerializer(serializers.ModelSerializer):
     date = serializers.DateField(format="%d/%m/%Y", input_formats=["%d/%m/%Y"])
+    clientName = serializers.SerializerMethodField()
+    collaboratorName = serializers.SerializerMethodField()
+    serviceName = serializers.CharField(source='service.name', read_only=True)
+
     class Meta:
         model = PaymentHistory
-        fields = '__all__'
+        fields = ['date', 'time', 'service', 'client', 'collaborator', 'price', 'is_payed', 'clientName', 'collaboratorName', 'serviceName']
+
+    def get_clientName(self, obj):
+        client = obj.client
+        # Concatenando name y lastname de Client
+        return f"{client.name} {client.lastname}" if client else None
+    
+    def get_collaboratorName(self, obj):
+        client = obj.client
+        # Concatenando name y lastname de Client
+        return f"{client.name} {client.lastname}" if client else None
