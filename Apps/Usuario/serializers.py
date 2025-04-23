4c65d4440c09cafa80f7b489151a781094f79a4e
Sebastian AR <crownedax@gmail.com>
2025-04-23 11:33:18 -0500

    ⚡️ Se añadio nombre comercial a datos simples

--- Apps/Usuario/serializers.py
+++ Apps/Usuario/serializers.py
@@ -1,4 +1,5 @@
-from .models import User, SocialNetwork
+from .models import User, SocialNetwork, CustomPermission, Role
+from ..Clientes.models import Client
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import check_password
@@ -26,11 +27,20 @@ class SimpleUserSerializer(serializers.ModelSerializer):
     fullname = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['email', 'fullname', 'position', 'photo']
+        fields = ['email', 'fullname', 'rol', 'photo']
 
     def get_fullname(self, obj):
         return f"{obj.name} {obj.lastname}"
+    
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
 
+        if instance.rol and instance.rol.is_staff:
+            client = Client.objects.filter(email=instance.email).first()
+            data['company_name'] = client.company_name if client else 'OkWeb'
+
+        return data
+    
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
@@ -58,3 +68,27 @@ class PasswordResetSerializer(serializers.Serializer):
         if 'new_password' not in data:
             raise serializers.ValidationError("Se requiere una nueva contraseña.")
         return data
+    
+class CustomPermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomPermission
+        fields = ['id', 'code']
+
+class RoleSerializer(serializers.ModelSerializer):
+    permissions = CustomPermissionSerializer(many=True, read_only=True)
+    permission_ids = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=CustomPermission.objects.all(), write_only=True, source='permissions'
+    )
+
+    class Meta:
+        model = Role
+        fields = ['id', 'name', 'is_staff', 'permissions', 'permission_ids']
+
+class UserRoleSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(read_only=True)
+    rol = RoleSerializer(read_only=True)
+    role_id = serializers.PrimaryKeyRelatedField(queryset=Role.objects.all(), write_only=True, source='rol')
+
+    class Meta:
+        model = User
+        fields = ['email', 'rol', 'role_id']
