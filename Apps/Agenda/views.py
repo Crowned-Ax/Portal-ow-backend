39a6e3fcff42e40c3631e1be5889438b9f4c7b44
Sebastian AR <crownedax@gmail.com>
2025-06-04 19:23:29 -0500

    ⚡️ Cambios en agenda asignar cliente

--- Apps/Agenda/views.py
+++ Apps/Agenda/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Schedule
 from ..Usuario.models import User
+from ..Clientes.models import Client
 from .serializers import ScheduleSerializer
 from django.core.exceptions import ValidationError
 from django.db.models import Q
@@ -12,17 +13,30 @@ class ScheduleViewSet(viewsets.ModelViewSet):
     serializer_class = ScheduleSerializer
 
     def get_queryset(self):
+        rol = User.objects.filter(email=self.request.user).first().rol.name
+        if(rol == "Super Admin" or rol == "Admin"):
+            return Schedule.objects.all()
         return Schedule.objects.filter(
             Q(created_by=self.request.user) | Q(assigned_to=self.request.user)
         )
 
     def perform_create(self, serializer):
         assigned_email = serializer.initial_data.get('assigned_to')
+        assigned_client_id = serializer.initial_data.get('assigned_client')
+        creador = self.request.user
+
+        if assigned_client_id and assigned_client_id != "":
+            assigned_client = Client.objects.filter(id=assigned_client_id).first()
+            if not assigned_client:
+                raise ValidationError("El cliente asignado no es válido.")
+            creador = assigned_client.user
+        else:
+            creador = User.objects.filter(email=self.request.user).first()
+
         if assigned_email and assigned_email != "":
             assigned_user = User.objects.filter(email=assigned_email).first()
             if not assigned_user:
                 raise ValidationError("El usuario asignado no es válido.")
-            creador = User.objects.filter(email=self.request.user).first()
             Notification.objects.create(
                 message=f"{creador.get_full_name()} te asigno una tarea",
                 date=timezone.now().date(),
@@ -33,21 +47,23 @@ class ScheduleViewSet(viewsets.ModelViewSet):
             assigned_user = self.request.user
 
         serializer.save(
-            created_by=self.request.user,
+            created_by=creador,
             assigned_to=assigned_user
         )
     
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
+        rol = User.objects.filter(email=self.request.user).first().rol.name
         # Solo el creador puede eliminar
-        if instance.created_by != request.user:
+        if instance.created_by != request.user and rol != "Super Admin" and rol != "Admin":
             return Response(status=status.HTTP_403_FORBIDDEN)
         return super().destroy(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
+        rol = User.objects.filter(email=self.request.user).first().rol.name
         # Solo el creador puede actualizar
-        if instance.created_by != request.user and instance.assigned_to != request.user:
+        if instance.created_by != request.user and instance.assigned_to != request.user and rol != "Super Admin" and rol != "Admin":
             return Response(status=status.HTTP_403_FORBIDDEN)
         return super().update(request, *args, **kwargs)
 
